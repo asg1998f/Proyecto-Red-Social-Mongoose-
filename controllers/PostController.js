2892fba551a7b2bf97b32760ad6924f9cdcc69c1
asg1998f <asg1998f@gmail.com>
2024-12-03 16:23:40 +0100

    endpoint create post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,7 +1,14 @@
 const Post = require("../models/Post");
 
 const PostContoller = {
-
+    async create(req,res){
+        try {
+            const post = await Post.create(req.body)
+            res.status(201).send({message:"New post successfully created",post})
+        } catch (error) {
+            res.status(500).send({message:"There was a problem"})    
+        }
+    }
 }
 
 module.exports = PostController
--- models/Post.js
+++ models/Post.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
     title: String,
+    content: String,
 }, { timestamps: true });
 
 const Post = mongoose.model('Post', PostSchema);
