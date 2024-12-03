dfb1d77a657602ab2758ce76a7773f6df8f703b6
karensalazar3 <karensalazar0503@gmail.com>
2024-12-03 15:22:05 +0100

    users

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router()
 const UserController = require('../controllers/UserController');
+const authentication = require("../middlewares/authentication");
 
 router.post('/',UserController.register)
 router.post('/login',UserController.login)
