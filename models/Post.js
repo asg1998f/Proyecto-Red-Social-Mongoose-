97d2fd84e2ead051f38c35328810727d1b5afd45
asg1998f <asg1998f@gmail.com>
2024-12-08 18:49:34 +0100

    validacion hecha

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
-const PostSchema = new mongoose.Schema({
-    title: String,
-    content: String,
-}, { timestamps: true });
+const PostSchema = new mongoose.Schema(
+    {
+    title: {
+        type:String,
+        required: [true, "Please, fill the title"],
+    },
+    content: {
+        type: String,
+        required: [true, "Please, fill the content"],
+    }, 
+},
+    { timestamps: true }
+);
 
 const Post = mongoose.model('Post', PostSchema);
 
