97408440c0d7f344136890b3fcc197eee842cc57
asg1998f <asg1998f@gmail.com>
2024-12-08 18:05:58 +0100

    endpoint paginacion

--- routes/posts.js
+++ routes/posts.js
@@ -8,5 +8,6 @@ router.put("/id/:_id",authentication,PostController.update)
 router.delete("/id/:_id",authentication,PostController.delete)
 router.get("/title/:title",PostController.getByTitle)
 router.get("/id/:_id",PostController.getById)
+router.get("/getAll",PostController.getAll)
 
 module.exports = router
