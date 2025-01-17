2f58f2023df42f81b575bdbed132da40c3013f8e
asg1998f <asg1998f@gmail.com>
2025-01-17 02:39:49 +0100

    addComment route

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -2,7 +2,7 @@ const User = require('../models/User.js');
 const jwt = require("jsonwebtoken")
 /* const {jwt_secret}=require("../config/keys.js") */
 require("dotenv").config()
-const PORT = process.env.PORT || 3001
+const jwt_secret = process.env.JWT_SECRET
 
 const authentication = async(req, res, next) => {
   try {
--- routes/posts.js
+++ routes/posts.js
@@ -11,6 +11,7 @@ router.get("/id/:_id",PostController.getById)
 router.get("/getAll",PostController.getAll)
 router.put("/like/:_id",authentication,PostController.like)
 router.put("/unlike/:_id",authentication,PostController.unlike)
+router.post("/addComment/:_id",authentication,PostController.addComment)
 
 
 module.exports = router
